7117c22b52fa8771588df89a864d764ae87c4e1b
shourya reddy kommula <shouryak12@shouryas-MacBook-Pro.local>
2022-09-21 21:00:21 -0400

    Updated New Changes

---
+++ springboot-backend/src/main/java/com/EMS/springboot/SpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.EMS.springboot.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+}
